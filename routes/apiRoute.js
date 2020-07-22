ea350acc1a5f90a8660603fd30d4b95aa9675db5
Aldo Patino <aldopguido@gmail.com>
2020-07-22 01:02:41 -0500

    dried some of the code up

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,12 +1,23 @@
 var noteData = require("../db/db.json");
+var fs = require("fs");
+var util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = function(app) {
-app.get("/api/Notes", function(req, res) {
-    return res.json(noteData);
+app.get("/api/notes", function(req, res) {
+ return readFileAsync("../db/db.json", "utf8")
+  .then(note => {
+    console.log(note)
+     res.json(note)
+ })
+  .catch(err => console.log(err)) 
+  
   });
 
   // Create New Characters - takes in JSON input
-app.post("/api/Notes", function(req, res) {
+app.post("/api/notes", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newNote = req.body;
--- server.js
+++ server.js
@@ -11,22 +11,25 @@ var PORT = process.env.PORT || 7000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"))
 // ================================================================================
-
+//API routes
+require("./routes/apiRoute")(app);
+  
+// ================================================================================
 //html routes
+
    //Home route
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
-   // API route
+   // note route
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
 
-//API routes
-require("./routes/apiRoute")(app);
+
 
 // =============================================================================
 
